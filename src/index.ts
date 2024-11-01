44149f0a33e8268b658326411a0a005cfd73d52a
WK <a.yambulatov@yandex.ru>
2024-11-01 22:26:16 +0400

    tst

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
 import './scss/styles.scss';
 
-//test
+interface InterfaceBasketModel {
+	contentItems: Map<string, number>;
+	addItem(id: string): void;
+	removeItem(id: string): void;
+}
+
+class BasketModel implements InterfaceBasketModel {
+	contentItems: Map<string, number> = new Map();
+
+	addItem(id: string): void {
+		try {
+			if (!this.contentItems.has(id)) {
+				this.contentItems.set(id, 0);
+			}
+
+			this.contentItems.set(id, this.contentItems.get(id) + 1);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	removeItem(id: string): void {
+		try {
+      
+			if (this.contentItems.get(id)! > 0) {
+				this.contentItems.set(id, this.contentItems.get(id) - 1);
+				if (this.contentItems.get(id) === 0) {
+					this.contentItems.delete(id);
+				}
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
