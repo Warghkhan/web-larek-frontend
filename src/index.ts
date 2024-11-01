2d21e285b1330d61f6287ac3bf1d187c1f12abb3
WK <a.yambulatov@yandex.ru>
2024-11-01 23:05:21 +0400

    tst

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,28 @@ interface InterfaceBasketModel {
 	removeItem(id: string): void;
 }
 
+interface InterfaceEventDispatcher {
+	dispatch: (event: string, data: unknown) => void;
+}
+
+interface InterfaceProductInfo {
+  id: string;
+  productTitle: string;
+}
+
+interface InterfaceCatalogueModel {
+  contentItems: InterfaceProductInfo[];
+  setContentItems(contentItems: InterfaceProductInfo[]): void;
+  getContentItem(id: string): InterfaceProductInfo;
+
+
+}
+
 class BasketModel implements InterfaceBasketModel {
 	contentItems: Map<string, number> = new Map();
+  constructor(protected events: InterfaceEventDispatcher) {
+
+  }
 
 	addItem(id: string): void {
 		try {
@@ -16,21 +36,30 @@ class BasketModel implements InterfaceBasketModel {
 			}
 
 			this.contentItems.set(id, this.contentItems.get(id) + 1);
+      this.changeBasketState();
 		} catch (err) {
 			console.log(err);
 		}
 	}
 	removeItem(id: string): void {
 		try {
-      
+
 			if (this.contentItems.get(id)! > 0) {
 				this.contentItems.set(id, this.contentItems.get(id) - 1);
 				if (this.contentItems.get(id) === 0) {
 					this.contentItems.delete(id);
 				}
 			}
+      this.changeBasketState();
 		} catch (err) {
 			console.log(err);
 		}
 	}
+
+  protected changeBasketState () {
+    this.events.dispatch('basket:change', {
+      contentItems: Array.from(this.contentItems.keys())
+    });
+  }
+
 }
